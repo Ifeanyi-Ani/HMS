a4e1e8327033d194f7d72f48d664b187589b2afb
IyiolaAfo <solomonafolayan95@gmail.com>
2023-09-14 14:46:58 +0100

    adding navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,39 @@
-function App() {
-  return <div className="App">Happy Hacking</div>;
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Nav from "./components/Navbar/Nav";
+// import Home from './components/pages/Home';
+// import About from './components/pages/About';
+// import Services from './components/pages/Services';
+// import Blog from './components/pages/Blog';
+// import Contact from './components/pages/Contact';
+// import Counter from "./components/Counter";
+// import Product from "./components/pages/Product";
+// import Users from "./components/Users";
+// import SingleUser from "./components/SingleUser";
+// import Registration from "./components/pages/Registration";
+// import Login from "./components/pages/login";
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        {/* <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/registration" element={<Registration />} /> */}
+        {/* <Route path="/login" element={<Login />} /> */}
+        {/* <Route path="/users/:id" element={<SingleUser />} />
+        <Route path="/counter" element={<Counter />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/contact" element={<Contact />} /> */}
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
 export default App;
+
