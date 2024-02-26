a11d09809b55028c29f3c70d29b0faf1528efdee
Ifeani-Ani <ifeanyiani155@gmail.com>
2024-02-26 12:42:47 +0100

    feat: add admin restriction see: #32

--- server/routes/users/adminRoutes.js
+++ server/routes/users/adminRoutes.js
@@ -1,7 +1,18 @@
-const express = require('express');
-const { createAdmin } = require('../../controllers/authControllers');
-const { protect, restrictTo } = require('../../middlewares/authMiddlewares');
+const express = require("express");
+const { createAdmin } = require("../../controllers/authControllers");
+const {
+  getAdmin,
+  deleteAdmin,
+  editAdmin,
+} = require("../../controllers/users/adminControllers");
+const { protect, restrictTo } = require("../../middlewares/authMiddlewares");
 const router = express.Router();
 
-router.route('/').post(protect, restrictTo('superadmin'), createAdmin);
+router
+  .route("/")
+  .post(protect, restrictTo("superadmin"), createAdmin)
+  .route("/:id")
+  .get(protect, restrictTo("admin"), getAdmin)
+  .patch(protect, restrictTo("admin"), editAdmin)
+  .delete(protect, restrictTo("superadmin", "admin"), deleteAdmin);
 module.exports = router;
--- server/routes/users/nurseRoutes.js
+++ server/routes/users/nurseRoutes.js
@@ -1,16 +1,23 @@
-const express = require('express');
+const express = require("express");
 
 const {
   getNurses,
   editNurse,
   deleteNurse,
   getNurse,
-} = require('../../controllers/users/nurseControllers');
-const { createNurse } = require('../../controllers/authControllers');
-const {protect,restrictTo}=require('../../middlewares/authMiddlewares')
+} = require("../../controllers/users/nurseControllers");
+const { createNurse } = require("../../controllers/authControllers");
+const { protect, restrictTo } = require("../../middlewares/authMiddlewares");
 
 const router = express.Router();
 
-router.route('/').get(getNurses).post(createNurse);
-router.route('/:id').get(protect,restrictTo('nurse'),getNurse).patch(editNurse).delete(deleteNurse);
+router
+  .route("/")
+  .get(getNurses)
+  .post(protect, restrictTo("admin"), createNurse);
+router
+  .route("/:id")
+  .get(protect, restrictTo("nurse"), getNurse)
+  .patch(editNurse)
+  .delete(deleteNurse);
 module.exports = router;
